c9f198fb3455a498ddcf6b528d14fc3df6f41d87
Andrew Cheam <cheamandrew@gmail.com>
2025-03-02 21:43:12 +0800

    update requirements and test

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 def test_data(availability_df, skills_df, jobs_df):
 
     print('Testing Data Quality...')
-    # Test that availability_df has name columns
+    
+    print("Checking if names appear in availability file")
     assert('Names' in availability_df.columns), "Ensure that Names column is in date_availability.csv"
-    # Test that availability_df has name columns
+    
+    print("Checking if names appear in skills_mapping file")
     assert('Names' in skills_df.columns), "Ensure that Names column is in skills_mapping.csv"
 
-    # Test that availability_df and skills_df have the same names
+    print("Checking that Names in both files match")
     assert(set(availability_df['Names']) == set(skills_df['Names'])), "Date availability CSV needs to have same member names as Skills CSV!"
-    # Test that jobs_df and skills_df have the same jobs
+    
+    print("Checking that Jobs in both files match")
     assert(set(jobs_df['Jobs']) == (set(skills_df.columns) - {'Names'})), "Jobs CSV needs to have same jobs as Skills CSV!"
 
     print('Data Quality is great!')
