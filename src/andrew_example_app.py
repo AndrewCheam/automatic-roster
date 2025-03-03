b55f0f8d1dda661f4b86dbb064fd9d0296e54340
Isaac Tan <78432772+isaactanjh@users.noreply.github.com>
2025-03-03 22:35:53 +0800

    error catching with try except and error message

--- src/andrew_example_app.py
+++ src/andrew_example_app.py
@@ -14,11 +14,16 @@ def convert_df_to_csv(df):
     output.seek(0)
     return output
 
+
+    
+
+
 st.title("Church Duties Scheduling Tool")
 
 date_availability_file = st.file_uploader("Upload Availability File", type=["csv", "xlsx", "xls"], key=1)
 skills_mapping_file = st.file_uploader("Upload Skills File", type=["csv", "xlsx", "xls"], key=2)
 jobs_file = st.file_uploader("Upload Jobs File", type=["csv", "xlsx", "xls"], key=3)
+
 
 
 if date_availability_file and skills_mapping_file and jobs_file:
@@ -31,14 +36,23 @@ if date_availability_file and skills_mapping_file and jobs_file:
     # st.write("### Availability CSV:", availability_df)
     # st.write("### Skills CSV:", skills_df)
     # st.write("### Jobs CSV:", jobs_df)
-
+    try:
     # Reset file pointers before passing to another function, if not the file 'gets consumed the first time'
-    date_availability_file.seek(0)
-    skills_mapping_file.seek(0)
-    jobs_file.seek(0)
-    
-    processed_df = process_csv(date_availability_file, skills_mapping_file, jobs_file)
-    st.write("### Processed CSV:", processed_df)
-    
-    csv_data = convert_df_to_csv(processed_df)
-    st.download_button(label="Download Processed CSV", data=csv_data, file_name="processed_schedule.csv", mime="text/csv")
+        date_availability_file.seek(0)
+        skills_mapping_file.seek(0)
+        jobs_file.seek(0)
+        
+        processed_df = process_csv(date_availability_file, skills_mapping_file, jobs_file)
+        st.write("### Processed CSV:", processed_df)
+        
+        csv_data = convert_df_to_csv(processed_df)
+        
+        st.download_button(label="Download Processed CSV", data=csv_data, file_name="processed_schedule.csv", mime="text/csv")
+    except Exception as e:
+        # If an error occurs, display the error message and retry option
+        st.error(f"**An error occurred while processing the files.**\n\nError details: {e}")
+        st.write("Double check the input files and try again or contact the owner.")
+        st.write("If the issue persists, please reach out to Andrew")
+        # Display retry button
+
+
